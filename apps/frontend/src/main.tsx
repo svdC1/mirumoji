d33b2a42796871c8040054a64f4631408dfa64d1
svdc <svdC1mail@gmail.com>
2025-06-29 15:45:21 -0300

    Implement Subtitle Style Customization

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,13 +5,16 @@ import App from "./App";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { ProfileProvider } from "./contexts/ProfileContext";
+import { SubtitleSettingsProvider } from "./contexts/SubtitleSettingsContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ProfileProvider>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <SubtitleSettingsProvider>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </SubtitleSettingsProvider>
         </ProfileProvider>
         <Toaster position="top-center" />
     </React.StrictMode>
