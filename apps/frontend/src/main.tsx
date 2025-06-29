1ce7b728b0ff8b071cf1f5e2999e1095ee7640e5
svdc <svdC1mail@gmail.com>
2025-06-29 16:39:20 -0300

    Fix Not Found Page being displayed in GitHub Pages

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ProfileProvider>
             <SubtitleSettingsProvider>
-                <BrowserRouter>
+                <BrowserRouter basename={import.meta.env.BASE_URL}>
                     <App />
                 </BrowserRouter>
             </SubtitleSettingsProvider>
